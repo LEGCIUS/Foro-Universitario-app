a9d6167997c696e14743a61b091bdc323f3f4bc5
LEGCIUS <danielrb4567@gmail.com>
2025-08-05 21:45:29 -0600

    Agregue una nueva clase llamada Perfil Usuario

---
+++ Clases/PerfilUsuario.js
@@ -0,0 +1,19 @@
+/**
+Foto de perfil
+Nombre completo
+Nombre de usuario / alias
+Carrera o especialidad
+Carné o número de estudiante
+Universidad o institución
+Nivel académico (ej. 1er año, 2do año, egresado)
+Fecha de nacimiento (si es relevante)
+Biografía corta o descripción personal
+Estado actual (en línea / desconectado / estudiando, etc.) talvez
+algo estilo rangos de discord por si un estudiantes es guia o para los profesores
+
+Actividad en la App
+Publicaciones realizadas (si es tipo red social)
+Comentarios o participaciones en foros o grupos
+Clases o eventos marcados como favoritos
+Mensajes o notificaciones recientes
+*/
